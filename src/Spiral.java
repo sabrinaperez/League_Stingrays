c176232c325d471cc0affa164fceaa02e75a3d17
sabrinaperez <sabrina.perez@jointheleague.org>
2017-02-28 12:53:43 -0800

    Update Spiral.java

--- src/Spiral.java
+++ src/Spiral.java
@@ -1,4 +1,26 @@
+import org.jointheleague.graphical.robot.Robot;
 
 public class Spiral {
 
+	public static void main(String[] args) {
+		// 1. Create a new Robot
+		
+		// 5. Set your robot's pen to the down position
+
+		// 3. Set the robot to go at max speed (10)
+
+		// 4. Repeat the code below 75 times (you’ll need to move the mustache!)
+
+			// 7. Change the pen color to random
+	
+			// 6. Move the robot 5 times the current line number you are drawing (5*i)
+	
+			// 2. Turn the robot 1/3 of 360 degrees to the right
+	
+			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
+	
+			// 9. Set the pen width to i
+
+	}
 }
+
