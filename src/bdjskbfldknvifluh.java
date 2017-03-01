544d8dbfc74729c02e511a24a14466cafb01f2e6
joshpryor <league@Admins-Mac-mini.local>
2017-02-28 16:56:01 -0800

    we made robots

---
+++ src/bdjskbfldknvifluh.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class bdjskbfldknvifluh {
+	public static void main(String[] args) {
+		Robot r2d2=new Robot
+				r2d2.setSpeed(100);
+		r2d2.penDown();
+		r2d2.penUp();
+		r2d2.turn(360/4*3);
+		r2d2.move(380);
+		r2d2.penDown();
+		r2d2.setPenColor(Color.magenta);
+		r2d2.turn(360/4*1/2);
+		r2d2.move(70);
+		r2d2.turn(360/4*1);
+	}
+
+}
